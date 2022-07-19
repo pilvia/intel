7ff0db00def789e551b7d048f475e1ef9e55e531
Sami Niemi <sami.niemi@pilvia.com>
2022-07-19 11:32:49 +0300

    working KV save

--- app/routes/endpoint.tsx
+++ app/routes/endpoint.tsx
@@ -1,25 +1,17 @@
 import { ActionFunction, json } from "@remix-run/cloudflare";
 
+export const action: ActionFunction = async ({ request, context }) => {
+  const data:any = await request.json();
+  
+  // SAVE to KV
 
-
-export const action: ActionFunction = async ({ request }) => {
-  const data = await request.json();
-  // KV save here
   if (request.method == "POST") {
-    console.log(data)
-    return json({status: 'ok'});
+    const db:KVNamespace = context.intelStorage
+    db.put(data.id, JSON.stringify(data))
+    return json({status: 'ok, thanks!'});
   } else {
     throw new Error('Wrong method');
   }
   
 };
 
-export async function loader({ }) {
-    let apiUrl = "https://raw.githubusercontent.com/SMAPPNYU/ProgrammerGroup/master/LargeDataSets/sample-tweet.raw.json";
-    let res = await fetch(apiUrl);
-  
-    let data = await res.json();
-  
-
-    return json(data);
-  }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,40 @@
-import type { LoaderFunction } from "@remix-run/cloudflare"; 
+import { json, LoaderFunction } from "@remix-run/cloudflare"; 
+import { useLoaderData } from "@remix-run/react";
+
+
+export const loader: LoaderFunction = async({ context }) => {
+  const db:KVNamespace = context.intelStorage
+ 
+  let data = await db.list();
+  const list = []
+  
+  for (let key of data.keys) {
+    const data = await db.get(key.name)
+    if (data) {
+      const jsonData = JSON.parse(data);
+      list.push({
+        id: key.name,
+        data: jsonData
+      }
+      );
+    }
+    
+  }
+  console.log(list);
+  return json(list);
+}
 
 export default function Index() {
+  
+  const hosts = useLoaderData()
   return (
+    <>
     <h1>Pilvia Intel</h1>
+    <ul>
+    {hosts.map((host:any) => (
+        <li key={host.id}>{host.id}: {host.data.hostname}</li>
+      ))}
+    </ul>
+    </>
   );
 }
